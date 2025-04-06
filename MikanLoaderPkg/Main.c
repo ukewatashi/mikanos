dbe5e83f21674ab296e5fd09e0e4f611d7ce027e
haru <t21b231a@mail.cc.niigata-u.ac.jp>
2025-04-06 18:24:07 +0900

    a

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -4,7 +4,7 @@
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
     EFI_SYSTEM_TABLE *system_table) {
-  Print(L"Hllo, Mikan World!\n");
+  Print(L"Hlo, Mikan World!\n");
   while (1);
   return EFI_SUCCESS;
 }
