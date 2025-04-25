28e3cede3eca04d73632a3af8812a99ac7d2360b
haru <t21b231a@mail.cc.niigata-u.ac.jp>
2025-04-25 11:16:38 +0900

    pixsel

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -18,16 +18,20 @@ struct PixelColor {
  * @retval 0   成功
  * @retval 非0 失敗
  */
+//  指定したｘｙにｃを書く
 int WritePixel(const FrameBufferConfig& config,
                int x, int y, const PixelColor& c) {
+　//　座標を連続の値に変換　はじからのアドレス　左上が００
   const int pixel_position = config.pixels_per_scan_line * y + x;
+  //　フォーマット判定無駄らしい
   if (config.pixel_format == kPixelRGBResv8BitPerColor) {
-    uint8_t* p = &config.frame_buffer[4 * pixel_position];
+    //　１ピクセルには４バイト分のメモリが割り当てられている
+    uint8_t* p = &config.frame_buffer[8 * pixel_position];
     p[0] = c.r;
     p[1] = c.g;
     p[2] = c.b;
   } else if (config.pixel_format == kPixelBGRResv8BitPerColor) {
-    uint8_t* p = &config.frame_buffer[4 * pixel_position];
+    uint8_t* p = &config.frame_buffer[8 * pixel_position];
     p[0] = c.b;
     p[1] = c.g;
     p[2] = c.r;
